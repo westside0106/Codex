8216d3cac8205a5d6dac2108bc7ef2fd9be3e8da
syphilipp <philipp.lahn@web.de>
2025-06-06 01:20:53 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .example import run as example_run
-
-def get_plugins():
-    return ["example"]
-
-def run_plugin(name):
-    if name == "example":
-        return example_run()
-    return f"Plugin '{name}' nicht gefunden."
